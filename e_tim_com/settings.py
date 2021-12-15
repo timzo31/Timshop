cbeb7c6500969c4447d13415c901792a653134db
timzo31 <madty31@gmail.com>
2021-12-15 12:58:03 +0800

    Account verification, dashboard, forgot & reset password email

--- e_tim_com/settings.py
+++ e_tim_com/settings.py
@@ -138,6 +138,13 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'madty31@gmail.com'
+EMAIL_HOST_PASSWORD = 'W@w@d0u O7'
+EMAIL_USE_TLS = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
